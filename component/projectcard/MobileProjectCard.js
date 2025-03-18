040359fcc92e0e78a3f2082ddeb9631c4d02d505
eonsyn <eonsync0@gmail.com>
2025-03-18 21:58:12 +0530

    update the layout

--- component/projectcard/MobileProjectCard.js
+++ component/projectcard/MobileProjectCard.js
@@ -5,32 +5,35 @@ import Link from "next/link";
 function MobileProjectCard({ project }) {
   return (
     <Link href={project.link} target="_blank" rel="noopener noreferrer">
-      <div className="block    relative px-2 md:hidden">
+      <div className="block  group  relative px-2 md:hidden">
         <div className="box absolute -bottom-1 right-2.5   ">
-          <div className="circle h-[18vw] w-[18vw] flex items-center justify-center bottom-0 right-0 rounded-full  bg-slate-600">
-            <FontAwesomeIcon className="rotate-45 text-5xl" icon={faArrowUp} />
+          <div className="circle h-[18vw] w-[18vw] flex items-center justify-center bottom-0 right-0 rounded-full  bg-red-300">
+            <FontAwesomeIcon
+              className="rotate-45 text-5xl text-black group-hover:rotate-90 transition-all duration-75 ease-in-out"
+              icon={faArrowUp}
+            />
           </div>
         </div>
         <div
-          className="absolute overflow-hidden top-0 rounded-3xl h-[60%] bg-purple-500"
+          className="absolute overflow-hidden top-0 rounded-t-3xl  h-[60%]  "
           style={{ width: "calc(100% - 1rem)" }}
         >
           <img
             src={project.imgUrl}
-            className="
-         w-full object-cover"
+            className=" 
+         w-full opacity-90 object-cover"
             alt=""
           />
         </div>
         <div
-          className="text   h-[40%]   absolute bottom-0 left-3"
+          className="text pl-1  text-black h-[40%]   absolute bottom-0 left-3"
           style={{ width: "calc(75% - 1rem)" }}
         >
-          <h1 className="text-3xl">{project.title}</h1>
-          <p className="tracking-wider leading-4">{project.description}</p>
+          <h1 className="text-4xl  pt-1 font-bold  ">{project.title}</h1>
+          <p className="tracking-wider   leading-4">{project.description}</p>
         </div>
         <img
-          src="/svgs/mobile card.svg"
+          src="/svgs/GreenCard.svg"
           className="w-full"
           alt="Mobile Project Card"
         />
